875618744cd2c2e1ec4d72d205921a1133b5251e
Genaro-B <genaroivanbusto@gmail.com>
2024-10-20 23:03:04 -0300

    Se agrega el contenido que no se estaba en Ej 5

--- Tp Java/Iterativas/src/Ej5/Ej5.java
+++ Tp Java/Iterativas/src/Ej5/Ej5.java
@@ -1,4 +1,25 @@
 package Ej5;
 
+import java.util.Scanner;
+
 public class Ej5 {
+    public static void main(String[] args){
+        Scanner leer=new Scanner(System.in);
+        int numeroEmpleados=0;
+        int horasTrabajo=0;
+        //Solicitud de datos:
+        System.out.println("Ingrese la cantidad de empleados que desea evaluar:");
+        numeroEmpleados=leer.nextInt();
+        do{
+            System.out.println("Indique la cantidad de horas de trabajo en la semana del empleado:"+numeroEmpleados);
+            horasTrabajo=leer.nextInt();
+            if(horasTrabajo>40){
+                System.out.println("Horas extras:"+(horasTrabajo-40));
+            }else{
+                System.out.println("El empleado realizó la cantidad justa o menos(no tiene horas extras)");
+            }
+            numeroEmpleados--;
+        }while (numeroEmpleados>0);
+        System.out.println("--FIN--");
+    }
 }
